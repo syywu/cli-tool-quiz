3dbbaa65f4ea620ccbe45fa0b842cbf973eba1b5
Samantha Wu <samanthayywu@gmail.com>
2022-06-04 19:32:26 +0100

    askName

--- src/cli.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import chalk from "chalk";
-import chalkAnimation from "chalk-animation";
-import figlet from "figlet";
-import gradient from "gradient-string";
-import inquirer from "inquirer";
-import { createSpinner } from "nanospinner";
---
+++ src/index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import gradient from "gradient-string";
+import inquirer from "inquirer";
+import { createSpinner } from "nanospinner";
+
+console.log(chalk.bgCyan("hello world"));
+
+await askName();
+
+async function askName() {
+  let playerName;
+  const name = await inquirer.prompt({
+    name: "playerName",
+    type: "input",
+    message: "What's your name?",
+    default() {
+      return "Player";
+    },
+  });
+  playerName = name.playerName;
+}
